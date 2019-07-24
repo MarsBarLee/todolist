7e71631703850ba0667f427f968395a4f400bc71
Mars Lee <jlee10@brockport.edu>
2019-07-24 13:49:58 -0400

    Updates

---
+++ precache-manifest.fe10288606eb4addd1473c05646c913b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583ca0ed71f3b34af64546168a97ee05",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "7035824da2de4968ac14",
+    "url": "/todolist/static/css/main.142a8262.chunk.css"
+  },
+  {
+    "revision": "c440897a4b826c529347",
+    "url": "/todolist/static/js/2.5caaee63.chunk.js"
+  },
+  {
+    "revision": "7035824da2de4968ac14",
+    "url": "/todolist/static/js/main.401a31e2.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
